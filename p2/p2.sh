4f7bad08b1383b1e2182f4ac1cf78ce3254839e9
cloud <cloud@terraforming-the-cloud.myguest.virtualbox.org>
2026-01-16 11:11:46 +0100

    I am dumb

--- p2/p2.sh
+++ p2/p2.sh
@@ -2,5 +2,5 @@
 docker ps --format '{{.Names}}'  | 
 while read -r line; do
     version=$(echo "$line" | sed -E 's/.*mrozniec-([0-9]).*/\1/')
-    docker exec -d $line -d ./host/host.sh "$version"
+    docker exec -d $line ./host/host.sh "$version"
 done
