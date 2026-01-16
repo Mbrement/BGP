52f2e7a684299128b22780f258f3f99828b86313
cloud <cloud@terraforming-the-cloud.myguest.virtualbox.org>
2026-01-16 11:28:48 +0100

    I am dumb, but I improve

--- p2/p2.sh
+++ p2/p2.sh
@@ -1,6 +1,20 @@
-#/bin/sh
-docker ps --format '{{.Names}}'  | 
+#!/bin/bash
+# docker ps --format '{{.Names}}'  
+echo "GNS3.routeur_mrozniec-2.bb3303b2-6023-4180-847b-e01ac021b7bd
+GNS3.host_mrozniec-2.bb3303b2-6023-4180-847b-e01ac021b7bd
+GNS3.routeur_mrozniec-1.bb3303b2-6023-4180-847b-e01ac021b7bd
+GNS3.host_mrozniec-1.bb3303b2-6023-4180-847b-e01ac021b7bd"| 
 while read -r line; do
     version=$(echo "$line" | sed -E 's/.*mrozniec-([0-9]).*/\1/')
-    docker exec -d $line ./host/host.sh "$version"
+    if [ -z "$version" ]; then
+        echo "Could not extract version from container name: $line"
+        continue
+    fi
+    if [[ "$line" == *"host"* ]]; then
+        docker exec -d $line ./host/host.sh "$version"
+    elif [[ "$line" == *"routeur"* ]]; then
+        docker exec -d $line ./routeur/static_routeur.sh "$version"
+    else
+        echo "Unknown container type for: $line"
+    fi
 done
--- p2/routeur/static_routeur.sh
+++ p2/routeur/static_routeur.sh
@@ -1,5 +1,15 @@
 #/bin/sh
-brctl addbr br0
-ip addr add 10.1.1.$id dev eth0
-brctl addif br0 eth1
-ifconfig br0 up
+routeur(){
+    id=$1
+    if [ -z "$id" ]; then
+        echo "Usage: routeur <id>"
+        return 1
+    fi
+    echo "Configuring routeur with ID: $id"
+    # brctl addbr br0
+    # ip addr add 10.1.1.$id dev eth0
+    # brctl addif br0 eth1
+    # ifconfig br0 up
+}
+
+routeur $1
